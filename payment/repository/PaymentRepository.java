afefe0529553c9e36d8869d2e7a0f0e407958e28
WilsonKuo543 <wilson820817@gmail.com>
2025-02-09 23:41:51 +0800

    Delete payment/repository/PaymentRepository.java

--- payment/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sportcenter.sportcenter.payment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sportcenter.sportcenter.payment.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Integer>, PaymentDAO {
-    Payment findByMerchantTradeNo(String merchantTradeNo);
-}
